e7a816a41041a6bc2f176673aeda990f6325aa63
Ubuntu <ubuntu@ip-172-31-68-156.ec2.internal>
2021-10-20 16:33:21 +0000

    rounding number 2

--- rounding_number.py
+++ rounding_number.py
@@ -5,19 +5,18 @@
 # This Program is about rounding number
 
 
-def round_number(number, decimal):
+def round_number(someVariable):
     # This function is about rounding number
 
     # process
-    number_float = number * 10 ** decimal + 0.5
+    number_float = someVariable[0] * 10 ** someVariable[1] + 0.5
     number_float_final = int(number_float)
-    number_output = number_float_final / 10 ** decimal
-
-    return number_output
+    someVariable[0] = number_float_final / 10 ** someVariable[1]
 
 
 def main():
     # This function just call other functions
+    someNumber = []
 
     # input
     user_number_string = input("Enter the number you want to round: ")
@@ -26,15 +25,14 @@ def main():
 
     try:
         user_number_number = float(user_number_string)
-        user_decimal_number = float(user_decimal_string)
+        user_decimal_number = int(user_decimal_string)
 
+        someNumber.append(user_number_number)
+        someNumber.append(user_decimal_number)
         # call functions
-        rounding_number = round_number(
-            number=user_number_number, decimal=user_decimal_number
-        )
-
+        round_number(someNumber)
         # output
-        print("The rounded number is {}".format(rounding_number))
+        print("The rounded number is {}".format(someNumber[0]))
 
     except Exception:
         # output
